b0069c114eb6cf64e7367597c628a813d1186f0a
Miriam <97313703+Mi-Olson@users.noreply.github.com>
2023-02-25 21:41:39 -0300

    corrigiendo responsive a 578px

--- scripts/up.js
+++ scripts/up.js
@@ -462,13 +462,13 @@ function create_cards(events) {
             card += `<div  class="card-group">
             <div class="card m-1" style="width: 15rem; ">
              <img src="${event.image}" class="card-img-top p-3" style="max-height: 7vh" alt="${event.category}">
-       <div class="card-body">
+       <div class="card-body ">
          <h5 class="card-title text-center" style="min-height: 7vh;">${event.name}</h5>
-         <p class="card-text text-center" style="min-height: 11vh;max-height: 11vh;" >${event.description}
+         <p class="card-text text-center scroller" style="min-height: 9vh;max-height: 9vh;" >${event.description}
          </p>
-         <div class="navbar card-footer">
-           <p >Price U$S ${event.price}</p>
-           <a href="./pages/details.html" class="btn btn-sm btn-new">More Info</a>
+         <div class="navbar card-footer ">
+           <p class=" mb-0">Price U$S ${event.price}</p>
+           <a href="./pages/details.html" class="btn btn-sm btn-new mt-1">More Info</a>
          </div>
        </div>
        </div>
