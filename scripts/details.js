35458c7bfb285b729329fbca90a688a225626d09
Miriam <97313703+Mi-Olson@users.noreply.github.com>
2023-03-03 18:48:24 -0300

    se erminaron de cargar los datos de details

--- scripts/details.js
+++ scripts/details.js
@@ -16,16 +16,20 @@ console.log(events);
 
 const div_card=document.getElementById("card_one")
 div_card.innerHTML=`<div class="row  row-details ">
-<img class="col-6 p-3 " src="${one_event.image}" alt="museum">
-<div class="col-4 text-center p-3 ">
-  <h2>Name</h2>
-  <p class="pt-4 ">Date: </p>
-  <p class="pt-4 ">Description: </p>
-  <p class="pt-4 ">Category: </p>
-  <p class="pt-4 ">place: </p>
-  <p class="pt-4 ">capacity: </p>
-  <p class="pt-4 ">assistence or estimate: </p>
-  <p class="pt-4 ">price: </p>
+<img class="col-6 p-3 " src="${one_event.image}" alt="${one_event.category}">
+<div class="col-6 text-center p-3 ">
+  <h1 class="line ">${one_event.name}</h1>
+  <p class="pt-4 h5 ">Date : ${one_event.date} </p>
+  <p class="pt-4 h5">${one_event.description} </p>
+  <div class="d-flex justify-content-around col-12">
+  <p class="pt-4 col-6">Category :${one_event.category} </p>
+  <p class="pt-4 col-6 ">place :${one_event.place} </p>
+  </div>
+  <div class="d-flex justify-content-around col-12">
+  <p class="pt-4 col-6">capacity :${one_event.capacity} </p>
+  <p class="pt-4 col-6">assistence or estimate :${one_event.assistance} </p>
+  </div>
+  <p class="pt-4 h5">price :${one_event.price} </p>
 </div>
 </div>`
   
